d37f0c8097ef1d5ab08fb88b095bf264e925dd94
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-08 01:43:57 +0530

    test

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ class NLPClassifier(object):
         if config["checkpoint"] != "":
             self._load(config["checkpoint"])
         self.curr_epoch = config["curr_epoch"]
-        self.name = "{}-{}-{}".format(config["model_name"].split("/")[1], config["batch_size"], config["learning_rate"])
+        self.name = "{}-{}-{}".format(config["model_name"], config["batch_size"], config["learning_rate"])
         self.bs = config["batch_size"]
         self.writer = SummaryWriter(log_dir="logs/{}".format(self.name))
         self.writer.flush()
