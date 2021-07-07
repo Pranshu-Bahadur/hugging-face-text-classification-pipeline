9d29d6ce68de33f4d5597e26fa1da6b36a2eebf7
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-08 01:25:22 +0530

    bigbird

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn as nn
 from torch.utils.tensorboard import SummaryWriter
-from transformers import AutoModel, AutoConfig, AutoTokenizer, SqueezeBertForSequenceClassification
+from transformers import AutoModel, AutoConfig, AutoTokenizer, SqueezeBertForSequenceClassification,  BigBirdModel
 from nfnets import SGD_AGC
 from sam import SAMSGD
 from sklearn.metrics import f1_score
@@ -30,10 +30,10 @@ class NLPClassifier(object):
         
     def _create_model(self, library, model_name, tokenizer, num_classes):
         if library == "hugging-face":
-            return SqueezeBertForSequenceClassification.from_pretrained(model_name, num_labels=num_classes), AutoTokenizer.from_pretrained(tokenizer)
+            return BigBirdModel.from_pretrained(model_name, num_labels=num_classes), AutoTokenizer.from_pretrained(tokenizer)
 
     def _create_optimizer(self, name, model_params, lr):
-        optim_dict = {"SGD":torch.optim.SGD(model_params.parameters(), lr,weight_decay=1e-5, momentum=0.9, nesterov=True),
+        optim_dict = {"SGD":torch.optim.SGD(model_params.parameters(), lr,weight_decay=1e-5, momentum=0.9),#, nesterov=True
                       "ADAM": torch.optim.Adam(model_params.parameters(), lr, betas=(0.9, 0.999)),
                       "SGDAGC": SGD_AGC(model_params.parameters(), lr=lr, clipping=0.01, weight_decay=1e-05, nesterov=True, momentum=0.9),
                       "SAMSGD": SAMSGD(model_params.parameters(), lr, momentum=0.9,weight_decay=1e-5)
@@ -79,10 +79,10 @@ class NLPClassifier(object):
             am = batch["attention_mask"].float().cuda()
             am[:, indices!=k] = 0
             y = batch['labels'].cuda()
-            outputs = self.model.forward(x,  attention_mask=am).logits
+            outputs = self.model(x,  attention_mask=am).logits
             loss = self.criterion(outputs, y)
             loss.backward()
-            
+            """
             if True:
                 def closure():
                     self.optimizer.zero_grad()
@@ -92,8 +92,8 @@ class NLPClassifier(object):
                     return loss
                 self.optimizer.zero_grad()
                 loss = self.optimizer.step(closure)
-            
-            #self.optimizer.step()
+            """
+            self.optimizer.step()
             self.scheduler.step()
             running_loss += loss.item()
             y_ = torch.argmax(outputs, dim=1)
