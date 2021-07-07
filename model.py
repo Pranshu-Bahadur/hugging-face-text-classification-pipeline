7694427ba5612af4ac3b371150f2ace7b1681f7f
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-07 19:58:52 +0530

    test

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class NLPClassifier(object):
     def __init__(self, config : dict):
-        self.model, self.tokenizer = self._create_model(config["library"], config["model_name"], config['tokenizer'], config["num_classes"])
+        self.model, self.tokenizer = self._create_model(config["library"], config["model_name"], config['tokenizer_name'], config["num_classes"])
         if config["train"]:
             self.optimizer = self._create_optimizer(config["optimizer_name"], self.model, config["learning_rate"])
             self.scheduler = self._create_scheduler(config["scheduler_name"], self.optimizer)
