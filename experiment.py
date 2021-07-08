e1ee2dc9b788678d002cc01c8e0c15b82020eef6
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-08 17:28:03 +0530

    te

--- experiment.py
+++ experiment.py
@@ -48,7 +48,7 @@ class Experiment(object):
         return dataSetFolder
     #@TODO...improve this...
     def _features_selection(self, loader):
-        self.classifier.model.eval()
+        self.classifier.model.train()
         X = np.concatenate(tuple([data["input_ids"].cpu().numpy() for data in loader]), axis=0)
         print(X.shape)
         K = 2
