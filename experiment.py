6abb2efa0120feeba567bb2dc1fd725d2664066e
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-08 18:06:57 +0530

    test

--- experiment.py
+++ experiment.py
@@ -48,7 +48,6 @@ class Experiment(object):
         return dataSetFolder
     #@TODO...improve this...
     def _features_selection(self, loader):
-        self.classifier.model.train()
         X = np.concatenate(tuple([data["input_ids"].cpu().numpy() for data in loader]), axis=0)
         print(X.shape)
         K = 2
--- model.py
+++ model.py
@@ -130,7 +130,7 @@ class NLPClassifier(object):
         return data["attention_mask"].grad
         """
         print(data["attention_mask"].size())
-        return torch.autograd.functional.jacobian(lambda x: self.model(data["input_ids"],atttention_mask=x).logits, data["attention_mask"], create_graph=True)
+        return torch.autograd.functional.jacobian(lambda x: self.model(data["input_ids"],attention_mask=x).logits, data["attention_mask"], create_graph=True)
     
     #@TODO Improve this...its nasty.
     def _score(self, loader, indices, k):
