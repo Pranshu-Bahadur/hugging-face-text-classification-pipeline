736f375f96afb5a56ea11f7ee057d94ba397c963
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-08 18:54:22 +0530

    test

--- model.py
+++ model.py
@@ -137,7 +137,7 @@ class NLPClassifier(object):
             ind_corr_matrix_score = {k: np.sum(np.log(np.absolute(np.corrcoef(v.cpu().numpy()+K)))) for k,v in list(per_class.items())}
             score = np.sum(np.absolute(list(ind_corr_matrix_score.values())))
             return score
-        J = self._get_jacobian(next(iter(loader), indices, k))
+        J = self._get_jacobian(next(iter(loader)), indices, k)
         return sum(list(map(lambda batch: eval_score_perclass(J.cuda(), batch['labels'].cuda()), [data for data in loader])))/1e+5
 
 
