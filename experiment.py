4e63599be12a6dfb9f1b5cd2ea5e3cd1c8ca9aa8
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-08 21:47:17 +0530

    test

--- experiment.py
+++ experiment.py
@@ -80,7 +80,7 @@ class Experiment(object):
                 #K += 2
                 continue
             s = max(list(map(lambda l_: l_[1],l)))
-            if s == 0:
+            if s == 0 or s == float('nan'):
                 print("max is 0...Reshuffling")
                 data = next(iter(loader))
                 X = data["input_ids"].cpu().numpy()
