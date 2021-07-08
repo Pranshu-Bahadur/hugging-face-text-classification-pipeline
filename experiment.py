daf8d8a48321a24eeffe628a7891ba41751bf002
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-08 22:49:09 +0530

    test

--- experiment.py
+++ experiment.py
@@ -17,12 +17,12 @@ class Experiment(object):
         init_epoch = self.classifier.curr_epoch
         loaders = [Loader(data, self.classifier.bs, shuffle=True, num_workers=4) for data in split]
         score = float('-inf')
-        K = 8
+        K = 2
         while (self.classifier.curr_epoch < init_epoch + config["epochs"]):
             print("Epoch {} Features selection with K {}:".format(self.classifier.curr_epoch+1, K), "--------------------")
             score_, k_, indices_ = self._features_selection(loaders[0], K)
             if score < score_:
-                K = K // 2 if K > 2 else 8
+                #K = K // 2 if K > 2 else 8
                 print("Better score updating features.")
                 score, k, indices = score_, k_, indices_
             print("Epoch {} Training Model based of newly selected features:".format(self.classifier.curr_epoch+1), "--------------------")
@@ -80,7 +80,7 @@ class Experiment(object):
             kmeans = KMeans(K, init="k-means++")
             indices = torch.tensor(kmeans.fit_predict(Z))
             clusters = {i: Z[indices==i].float().cuda() for i in range(K)}
-            big_c = max(list(map(lambda c: torch.mean(c),list(clusters.values()))))
+            big_c = torch.maximum(torch.stack(list(map(lambda c: torch.mean(c),list(clusters.values())))))
             clusters = list(filter(lambda k: torch.mean(clusters[k])==big_c, list(clusters.keys())))
             l = list(map(lambda idx: (idx, self.classifier._score(data, indices, idx)), [i for i in range(K)]))#clusters
             l = list(filter(lambda a_: float('nan') != a_[1], l))
