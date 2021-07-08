f6a1a1347b938d99ee58c9f45455a2c560c9b602
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-08 17:22:23 +0530

    done

--- model.py
+++ model.py
@@ -125,7 +125,7 @@ class NLPClassifier(object):
         data["attention_mask"] = data["attention_mask"].float()
         data["attention_mask"].requires_grad = True
         h = self.model(**data).loss
-        m = torch.zeros((h.size()))
+        m = torch.zeros((data["attention_mask"].size(0), self.nc))
         m[:, 0] = 1
         h.backward()
         return data["attention_mask"].grad
