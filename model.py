1c2b1e45797b62da6c1e0f14d41911375ddc1850
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-09 16:47:35 +0530

    test

--- model.py
+++ model.py
@@ -34,6 +34,7 @@ class NLPClassifier(object):
     def _create_model(self, library, model_name, num_classes):
         if library == "hugging-face":
             model = AutoModelForSequenceClassification.from_pretrained(model_name)
+            print(model)
             model.classifier = nn.Linear(in_features=model.classifier.in_features, out_features=num_classes, bias=True)
             model.num_labels = num_classes
             return model, AutoTokenizer.from_pretrained(model_name)
