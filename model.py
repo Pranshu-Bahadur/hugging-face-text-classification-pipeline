67b70a941f1c1e6fa6ad51033025c2a2c6597eca
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-10 16:45:05 +0530

    test

--- model.py
+++ model.py
@@ -190,7 +190,7 @@ class NLPClassifier(object):
         V_J, V_y = (J_n - torch.mean(J_n)), (y_n - torch.mean(y_n))
         print(V_J.size(), V_y.size())
         corr_m = torch.sum(V_J.T*V_y) / (torch.sqrt(torch.sum(V_J.T ** 2)) * torch.sqrt(torch.sum(V_y ** 2))).cpu()
-        corr_m.cpu().apply_(lambda x: torch.log(abs(x+k))
+        corr_m.cpu().apply_(lambda x: torch.log(abs(x)+k))
         return torch.sum(torch.abs(corr_m).view(-1)).item()
     
     #NOTE: Untested. Only Nlp
