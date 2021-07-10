accd4c8b755b72f0e85e729e440137834bb6f761
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-10 15:16:26 +0530

    test

--- model.py
+++ model.py
@@ -181,7 +181,7 @@ class NLPClassifier(object):
         X = X.view(X.size(0),-1)
         print(X.size(0))
         cluster_ids_x, cluster_centers = kmeans(X=X.T, num_clusters=2, device=torch.device('cuda:0'))
-        print(cluster_centers)
+        print(cluster_centers, cluster_ids_x)
         best_cluster_center, best_cluster = selection_heuristic(cluster_centers)
         return best_cluster.item(), best_cluster_center, cluster_ids_x
     
