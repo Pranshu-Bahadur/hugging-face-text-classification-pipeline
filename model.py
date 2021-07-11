0c50f9313e6d659b54de62589403dac596f041f8
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-11 14:03:43 +0530

    test

--- model.py
+++ model.py
@@ -147,7 +147,7 @@ class NLPClassifier(object):
     def _features_selection(self, K, loader, selection_heuristic=lambda x: torch.mode(x)):
         X = torch.cat([data["input_ids"] for data in loader][:-1])
         X = X.view(X.size(0), -1)
-        cluster_ids_x, cluster_centers = kmeans(X=X.T, num_clusters=2, device=torch.device('cuda:0'))
+        cluster_ids_x, cluster_centers = kmeans(X=X, num_clusters=2, device=torch.device('cuda:0'))
         best_cluster, cluster_ids_x = selection_heuristic(cluster_ids_x)
         print(best_cluster, cluster_centers[best_cluster], cluster_ids_x)
         return best_cluster, cluster_centers[best_cluster], cluster_ids_x
