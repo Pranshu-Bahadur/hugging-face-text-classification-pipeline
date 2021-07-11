64ff360e207549eb082842fcdaf4d1d3af01a84c
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-11 19:23:35 +0530

    batchwise weighted loss

--- model.py
+++ model.py
@@ -107,6 +107,7 @@ class NLPClassifier(object):
                 if self.score != float("-inf"):
                     data["attention_mask"][:,self.clusters_idx!=self.cluster_idx] = 0
                 outputs = self.model.forward(input_ids=data["input_ids"], attention_mask=data["attention_mask"]).logits
+                self.criterion.weight = torch.tensor([data["labels"][data["labels"]==i].size(0)/self.bs for i in range(16)])
                 loss = self.criterion(outputs.view(data["labels"].size(0), -1), data["labels"])
             self.optimizer.zero_grad()
             loss.backward()
