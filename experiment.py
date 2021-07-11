580568b28536a846bbff4fdbe26f3e5859c11d5b
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-11 21:54:57 +0530

    t

--- experiment.py
+++ experiment.py
@@ -45,8 +45,8 @@ class Experiment(object):
         topk, indices = torch.topk(torch.mean(cluster_centers, dim=-1), 8)
         print(indices)
         print("Result of k-means:",topk, cluster_centers[indices], cluster_ids_x)
-        print(torch.cat([cluster_ids_x==i for i in indices]))
-        dataSetFolder = dataSetFolder[list((cluster_ids_x==indices).non_zero(as_tuple=True))]
+        print((cluster_ids_x==indices).nonzero(as_tuple=True).tolist())
+        dataSetFolder = dataSetFolder[(cluster_ids_x==indices).nonzero(as_tuple=True).tolist()]
 
 
         #@TODO add features selection here
