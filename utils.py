824115c24bd35f8677d8e0a19100af230a663578
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-11 23:18:26 +0530

    test

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ class SpreadSheetNLPCustomDataset(Dataset):
         filter_links_phrases = ["https://", ".com", "http://", "youtube", "www"]
         self.dataset = pd.DataFrame(pd.concat([Series(row['type'], row['posts'].split("|||")) for _, row in self.dataset.iterrows()]).reset_index())
         [self.dataset.rename(columns = {name:cols_n[i]}, inplace = True) for i,name in enumerate(self.dataset.columns.tolist())]
-        self.dataset = self.dataset[self.dataset['posts'].str.split().str.len() > 32]
+        self.dataset = self.dataset[self.dataset['posts'].str.split(" ").str.len() > 32]
         self.dataset = self.dataset[~self.dataset['posts'].str.contains("|[a-z]".join(types) + "|[a-z]".join(types).lower() + "|[a-z]".join(filter_links_phrases))]
         self.dataset = self.dataset[~self.dataset['posts'].str.contains("|[0-9]".join(types) + "|[0-9]".join(types).lower() + "|[0-9]".join(filter_links_phrases))]
         self.dataset = self.dataset[~self.dataset['posts'].str.contains("[a-z]|".join(types) + "[a-z]|".join(types).lower() + "[a-z]|".join(filter_links_phrases))]
