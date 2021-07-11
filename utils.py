712b9989faa772b0da2a23b6f6204e6d9e37a21e
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-11 18:25:52 +0530

    te

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ class SpreadSheetNLPCustomDataset(Dataset):
         filter_links_phrases = ["https://", ".com", "http://"]
         self.dataset = pd.DataFrame(pd.concat([Series(row['type'], row['posts'].split("|||")) for _, row in self.dataset.iterrows()]).reset_index())
         [self.dataset.rename(columns = {name:cols_n[i]}, inplace = True) for i,name in enumerate(self.dataset.columns.tolist())]
-        self.dataset = self.dataset[self.dataset['posts'].map(len) >= 256//2]
+        self.dataset = self.dataset[self.dataset['posts'].map(len) >= 256]
         self.dataset = self.dataset[~self.dataset['posts'].str.contains("|".join(filter_links_phrases))]
         print(f"filter success {len(self.dataset)}")
         self.encodings = tokenizer(list(self.dataset['posts'].values), padding='max_length', max_length=256, truncation=True)
