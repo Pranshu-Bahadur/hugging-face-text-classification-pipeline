1753341ae47e790c5ec519347a227e698d84586b
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-11 18:03:04 +0530

    t

--- utils.py
+++ utils.py
@@ -18,8 +18,9 @@ class SpreadSheetNLPCustomDataset(Dataset):
         cols_n.reverse()
         #types = list(self.dataset.type.unique())
         filter_links_phrases = ["https://", ".com", "http://"]
-        self.dataset = pd.DataFrame(pd.concat([Series(row['type'], list(filter(lambda p: len(p) >= 256//4 and all(list(map(lambda phrase: not phrase in p,filter_links_phrases))), row['posts'].split("|||")))) for _, row in self.dataset.iterrows()]).reset_index())
+        self.dataset = pd.DataFrame(pd.concat([Series(row['type'], row['posts'].split("|||")) for _, row in self.dataset.iterrows()]).reset_index())
         [self.dataset.rename(columns = {name:cols_n[i]}, inplace = True) for i,name in enumerate(self.dataset.columns.tolist())]
+        self.dataset = self.dataset[len(self.dataset['posts']) >= 256//4 and all(list(map(lambda phrase: not phrase in self.dataset['posts'],filter_links_phrases)))]
         self.encodings = tokenizer(list(self.dataset['posts'].values), padding='max_length', max_length=256, truncation=True)
         self.labels = {k: v for v, k in enumerate(self.dataset.type.unique())}
         self.dataset['type'] = self.dataset['type'].apply(lambda x: self.labels[x])
