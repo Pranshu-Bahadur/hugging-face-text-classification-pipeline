21e94f3f7a52cbc5b5783d057f12657e649028a1
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-13 01:38:33 +0530

    test

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ class SpreadSheetNLPCustomDataset(Dataset):
         self.library = library
         cols_n = self.dataset.columns.tolist()
         cols_n.reverse()
-        types = list(self.dataset["type"].unique().apply(lambda x: x.lower()))
+        types = list(np.vectorize(lambda x: x.lower())(self.dataset["type"].unique()))
         filter_links_phrases = ["https://", ".com", "http://", "youtube", "www"]
         self.dataset = pd.DataFrame(pd.concat([Series(row['type'], row['posts'].split("|||")) for _, row in self.dataset.iterrows()]).reset_index())
         [self.dataset.rename(columns = {name:cols_n[i]}, inplace = True) for i,name in enumerate(self.dataset.columns.tolist())]
