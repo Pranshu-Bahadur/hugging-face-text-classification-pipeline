2c48590b6f556ead2f588f335be96ed8af6e030a
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-13 00:00:31 +0530

    test

--- model.py
+++ model.py
@@ -147,8 +147,8 @@ class NLPClassifier(object):
                 data = {k: v[shuffle_seed].cuda() for k, v in data.items()}
                 if self.score != float("-inf"):
                     data["attention_mask"][:,self.clusters_idx!=self.cluster_idx] = 0
-                a, outputs, _ = self.trainer.prediction_step(self.model, data, prediction_loss_only=False)
-                print(a.size())
+                _, outputs, _ = self.trainer.prediction_step(self.model, data, prediction_loss_only=False)
+                print(outputs.size())
                 loss = self.criterion(outputs.view(data["labels"].size(0), -1), data["labels"])
                 #outputs = self.model(input_ids=data["input_ids"], attention_mask=data["attention_mask"]).logits#, attention_mask=data["attention_mask"]
                 #self.criterion.weight = torch.tensor([self.criterion.weight[i]+(data["labels"][data["labels"]==i].size(0)/self.bs) for i in range(16)]).cuda()
