035fe287fae14653c49c5d8dcd83f3679b2bddce
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-13 01:46:30 +0530

    test

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ class SpreadSheetNLPCustomDataset(Dataset):
         #self.dataset = pd.DataFrame(pd.concat([Series(row['type'], row['posts'].split("|||")) for _, row in self.dataset.iterrows()]).reset_index())
         #[self.dataset.rename(columns = {name:cols_n[i]}, inplace = True) for i,name in enumerate(self.dataset.columns.tolist())]
         #self.dataset.posts = self.dataset["posts"].str.lower()
-        self.dataset = self.dataset[~self.dataset['posts'].str.contains("|".join(filter_links_phrases))]
+        #self.dataset = self.dataset[~self.dataset['posts'].str.contains("|".join(filter_links_phrases))]
         #self.dataset = self.dataset[~self.dataset['posts'].str.contains("|".join(types))]
         print(f"filter success {len(self.dataset)}")
         print(f"Tokenizing dataset...")
