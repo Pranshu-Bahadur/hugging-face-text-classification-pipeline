e1a48ef3b19a59f2451ecc5ac3edf4b20fab44da
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-12 23:58:20 +0530

    test

--- model.py
+++ model.py
@@ -147,7 +147,9 @@ class NLPClassifier(object):
                 data = {k: v[shuffle_seed].cuda() for k, v in data.items()}
                 if self.score != float("-inf"):
                     data["attention_mask"][:,self.clusters_idx!=self.cluster_idx] = 0
-                loss, outputs, _ = self.trainer.prediction_step(self.model, data, prediction_loss_only=False)
+                a, outputs, _ = self.trainer.prediction_step(self.model, data, prediction_loss_only=False)
+                print(a.size())
+                loss = self.criterion(outputs.view(data["labels"].size(0), -1), data["labels"])
                 #outputs = self.model(input_ids=data["input_ids"], attention_mask=data["attention_mask"]).logits#, attention_mask=data["attention_mask"]
                 #self.criterion.weight = torch.tensor([self.criterion.weight[i]+(data["labels"][data["labels"]==i].size(0)/self.bs) for i in range(16)]).cuda()
                 #loss = self.criterion(outputs.view(data["labels"].size(0), -1), data["labels"])
@@ -188,8 +190,9 @@ class NLPClassifier(object):
                     if self.score != float("-inf"):
                         data["attention_mask"][:,self.clusters_idx] = 0
                     #outputs = self.model(input_ids=data["input_ids"],attention_mask=data["attention_mask"]).logits# 
-                    #loss = self.criterion(outputs.view(data["labels"].size(0), -1), data["labels"])
-                    loss, outputs, _ = self.trainer.prediction_step(self.model, data,  prediction_loss_only=False)
+                    #
+                    _, outputs, _ = self.trainer.prediction_step(self.model, data,  prediction_loss_only=False)
+                    loss = self.criterion(outputs.view(data["labels"].size(0), -1), data["labels"])
 
                 running_loss += loss.cpu().item()
                 y_ = torch.argmax(outputs, dim=1)
