43e5f12c5a293f8ba5daf99a18d4a25821bd9344
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-13 02:01:53 +0530

    test wout r

--- utils.py
+++ utils.py
@@ -20,8 +20,9 @@ class SpreadSheetNLPCustomDataset(Dataset):
         filter_links_phrases = ["https://", ".com", "http://", "youtube", "www"]
         #self.dataset = pd.DataFrame(pd.concat([Series(row['type'], row['posts'].split("|||")) for _, row in self.dataset.iterrows()]).reset_index())
         #[self.dataset.rename(columns = {name:cols_n[i]}, inplace = True) for i,name in enumerate(self.dataset.columns.tolist())]
-        #self.dataset.posts = self.dataset["posts"].str.lower()
+        self.dataset.posts = self.dataset["posts"].str.lower()
         self.dataset['posts'] = self.dataset['posts'].str.replace(r'|||', ' ')
+        self.dataset['posts'] = self.dataset['posts'].str.replace('('+'|'.join(types)+')', ' ')
         self.dataset['posts'] = self.dataset['posts'].str.replace(r'\b^https://*.com$\b', ' ')
         self.dataset['posts'] = self.dataset['posts'].str.replace(r'\b^https://*.com$\b', ' ')
         print(self.dataset.head())
