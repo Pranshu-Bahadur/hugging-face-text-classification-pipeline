070722472702cc110b38d97bd60f6832ac7ec0b6
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-13 02:02:20 +0530

    test

--- utils.py
+++ utils.py
@@ -22,9 +22,9 @@ class SpreadSheetNLPCustomDataset(Dataset):
         #[self.dataset.rename(columns = {name:cols_n[i]}, inplace = True) for i,name in enumerate(self.dataset.columns.tolist())]
         self.dataset.posts = self.dataset["posts"].str.lower()
         self.dataset['posts'] = self.dataset['posts'].str.replace(r'|||', ' ')
-        self.dataset['posts'] = self.dataset['posts'].str.replace('('+'|'.join(types)+')', ' ')
-        self.dataset['posts'] = self.dataset['posts'].str.replace(r'\b^https://*.com$\b', ' ')
-        self.dataset['posts'] = self.dataset['posts'].str.replace(r'\b^https://*.com$\b', ' ')
+        self.dataset['posts'] = self.dataset['posts'].str.replace('('+'|'.join(types)+')', '')
+        self.dataset['posts'] = self.dataset['posts'].str.replace(r'\b^https://*.com$\b', '')
+        self.dataset['posts'] = self.dataset['posts'].str.replace(r'\b^https://*.com$\b', '')
         print(self.dataset.head())
         #self.dataset = self.dataset[~self.dataset['posts'].str.contains("|".join(types))]
         print(f"filter success {len(self.dataset)}")
