d663fba290b4b281246f3e115c5d51fcff13f9f0
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-14 00:53:30 +0530

    their stuff agaen

--- experiment.py
+++ experiment.py
@@ -20,7 +20,7 @@ class Experiment(object):
 
     def _run(self, dataset, config: dict):
         dataset, splits, indices = self._preprocessing(dataset, True)#, indices, Y_ 
-        random.shuffle(indices)
+        #random.shuffle(indices)
         init_epoch = self.classifier.curr_epoch
         
         training_args = TrainingArguments(output_dir='./results',
@@ -33,7 +33,7 @@ class Experiment(object):
          weight_decay=1e-5,
          logging_dir='./logs',
          logging_strategy="steps",
-         logging_steps=int((splits[0]//self.classifier.bs)*0.1),
+         logging_steps=int((len(splits[0])//self.classifier.bs)*0.1),
          #evaluation_strategy="steps",
          #eval_steps=int((len(splits[1])//self.classifier.bs)*0.1),
          #eval_accumulation_steps = 1
@@ -48,10 +48,11 @@ class Experiment(object):
         #weights = weights/np.sum(weights)*self.classifier.nc
         #self.classifier.criterion = nn.CrossEntropyLoss(weight= torch.tensor(weights).float().cuda()).cuda()
         #splits = [dataset[:indices]]
+        """
         loaders = [Loader(dataset, self.classifier.bs, shuffle=False, num_workers=4, sampler=indices[:splits[0]]),
         Loader(dataset, self.classifier.bs, shuffle=False, num_workers=4, sampler=indices[splits[1]:]),#, sampler=indices[splits[1]:]),
         Loader(dataset, self.classifier.bs, shuffle=False, num_workers=4, sampler=indices[splits[1]+splits[2]:])#, sampler=indices[splits[1]+splits[2]:]),
-        ]
+        ]"""
         
         print("Dataset has been preprocessed and randomly split.\nRunning training loop...\n")
         metric = load_metric("accuracy")
@@ -64,22 +65,23 @@ class Experiment(object):
         #print("\nRunning dimensoniality reduction...\nRunning training loop...\n")
         #self.classifier._k_means_approximation_one_step(loaders[0])
         """
-        trainer = Trainer(model=self.classifier.model, args=training_args)
+        trainer = Trainer(model=self.classifier.model, args=training_args, train_dataset=splits[0], compute_metrics=compute_metrics,)
         while (self.classifier.curr_epoch < init_epoch + config["epochs"]):
-            trainer.train(loader=loaders[0])
+            self.classifier.curr_epoch +=1
+            trainer.train()
             with torch.no_grad():
-                trainer.model = self.classifier.model
-                trainer.model.eval()
-                print(trainer.evaluation_loop(loaders[1],description="Validation split evaluation",prediction_loss_only=False))
+                print(trainer.evaluate(splits[1]))
             if self.classifier.curr_epoch%config["save_interval"]==0:
                 self.classifier._save(config["save_directory"], "{}-{}".format(self.classifier.name, self.classifier.curr_epoch))
+            trainer.state.epoch = self.classifier.curr_epoch
         print("\n\n")
-        print(trainer.evaluation_loop(loaders[0],description="Test split evaluation",prediction_loss_only=False))
+        print(trainer.evaluate(splits[2]))
         print("\nRun Complete.\n\n")
 
     def _preprocessing(self, directory, train):
         dataSetFolder = self.classifier.dataset       
         #loader = Loader(dataSetFolder, self.classifier.bs, shuffle=False, num_workers=4)
+        """
         print("\n\nRunning K-means for outlier detection...\n\n")
         X = torch.tensor(torch.tensor(dataSetFolder.encodings["input_ids"])).cuda()
         X = X.view(X.size(0), -1)
@@ -90,15 +92,17 @@ class Experiment(object):
         topk, indices = torch.topk(torch.tensor([(cluster_ids_x==i).nonzero().size(0) for i in range(8)]), 1)#torch.tensor([torch.mean(cluster_centers[i].float()).float() for i in range(8)]),2)#torch.tensor([(cluster_ids_x==i).nonzero().size(0) for i in range(8)]), 1)
         indices = torch.cat([(cluster_ids_x==i).nonzero() for i in indices], dim=0).view(-1).tolist()
         print(f"\n\nResult of k-means: {len(indices)} samples remain, taken from top 7 cluster(s)\n\n")
+        """
         #X_ = X[indices]
         #dist = [Y_[indices].cpu().size(0) for i in Y_.unique()]
+        indices = []
         #@TODO add features selection here
         if train:
             trainingValidationDatasetSize = int(0.6 * len(dataSetFolder))
             testDatasetSize = int(len(dataSetFolder) - trainingValidationDatasetSize) // 2
             #diff = len(dataSetFolder) - sum([trainingValidationDatasetSize, testDatasetSize, testDatasetSize+1])
-            splits = [trainingValidationDatasetSize, testDatasetSize, testDatasetSize]
-            #splits = torch.utils.data.dataset.random_split(dataSetFolder, splits)
+            splits = [trainingValidationDatasetSize, testDatasetSize, testDatasetSize+1]
+            splits = torch.utils.data.dataset.random_split(dataSetFolder, splits)
             #total = sum(list(dataSetFolder.distribution.values()))
             return dataSetFolder ,splits, indices#, Y
         return dataSetFolder
