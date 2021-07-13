61cd34fa0155986755e45626de407c2a78cdb332
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-13 05:43:38 +0530

    test

--- model.py
+++ model.py
@@ -186,18 +186,18 @@ class NLPClassifier(object):
                 if self.score != float("-inf"):
                     data["attention_mask"][:,self.clusters_idx!=self.cluster_idx] = 0
                 #data["labels"] = data["labels"].float()
-                _, outputs, _ = self.trainer.prediction_step(self.model, data, prediction_loss_only=False, ignore_keys=['labels'])
+                loss, outputs, _ = self.trainer.prediction_step(self.model, data, prediction_loss_only=False)
                 self.model.train()
 
-                loss = self.criterion(outputs.view(data["labels"].size(0), -1), data["labels"])
+                #loss = self.criterion(outputs.view(data["labels"].size(0), -1), data["labels"])
                 #print(loss.size())
                 loss.requires_grad = True
                 #outputs = self.model(input_ids=data["input_ids"], attention_mask=data["attention_mask"]).logits#, attention_mask=data["attention_mask"]
                 #self.criterion.weight = torch.tensor([self.criterion.weight[i]+(data["labels"][data["labels"]==i].size(0)/self.bs) for i in range(16)]).cuda()
                 #loss = self.criterion(outputs.view(data["labels"].size(0), -1), data["labels"])
             #print(outputs.size())
-            #self.scaler.scale(loss).backward()
-            loss.backward()
+            self.scaler.scale(loss).backward()
+            #loss.backward()
             running_loss += loss.cpu().item()
             self.optimizer.step()
             self.scheduler.step()
@@ -234,8 +234,8 @@ class NLPClassifier(object):
                         data["attention_mask"][:,self.clusters_idx] = 0
                     #outputs = self.model(input_ids=data["input_ids"],attention_mask=data["attention_mask"]).logits# 
                     #
-                    _, outputs, _ = self.trainer.prediction_step(self.model, data,  prediction_loss_only=False, ignore_keys=['labels'])
-                    loss = self.criterion(outputs.view(data["labels"].size(0), -1), data["labels"])
+                    loss, outputs, _ = self.trainer.prediction_step(self.model, data,  prediction_loss_only=False)
+                    #loss = self.criterion(outputs.view(data["labels"].size(0), -1), data["labels"])
 
                 running_loss += loss.cpu().item()
                 y_ = torch.argmax(outputs, dim=1)
