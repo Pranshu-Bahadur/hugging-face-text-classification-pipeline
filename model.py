782fcc1a052adb779867e2807ef9784dd0d4c81e
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-13 13:22:03 +0530

    test

--- model.py
+++ model.py
@@ -71,7 +71,7 @@ class NLPClassifier(object):
             model.num_labels = num_classes
             model.config.max_position_embeddings = 512
             model.batch_size = self.bs
-            #model.classifier = nn.Linear(in_features=model.classifier.in_features, out_features=16, bias=True)
+            model.classifier = nn.Linear(in_features=model.classifier.in_features, out_features=16, bias=True)
 
 
             print(model.config)
