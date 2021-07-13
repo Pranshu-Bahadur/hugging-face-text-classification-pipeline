d612b865e0e6b3f5b02e52b15d163dc57a0f7382
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-14 03:24:06 +0530

    customish training loop

--- experiment.py
+++ experiment.py
@@ -96,7 +96,7 @@ class Experiment(object):
             if self.classifier.curr_epoch%config["save_interval"]==0:
                 self.classifier._save(config["save_directory"], "{}-{}".format(self.classifier.name, self.classifier.curr_epoch))
             #trainer.state.num_train_epochs = 1
-        """
+        
         print("\n\n")
         print(trainer.evaluate(splits[2]))
         print("\nRun Complete.\n\n")
