d10d97191859349fd464847ef7877242f817334f
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-13 21:12:13 +0530

    test

--- model.py
+++ model.py
@@ -73,10 +73,6 @@ class NLPClassifier(object):
             config.embedding_size = 48
             config.classifier_dropout_prob = 0
             config.num_labels = num_classes
-            config.num_attention_heads = 2
-            config.num_hidden_layers = 4
-            config.hidden_dim = 64
-            config.dim = 128
             return config
         else:
             return timm.create_model(model_name, pretrained=True, num_classes=num_classes)
