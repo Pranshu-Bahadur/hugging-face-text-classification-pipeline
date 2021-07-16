cfa2cb311c41ca6bf6497dff7ebaca44a6532b05
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-17 04:31:19 +0530

    Weighted Random-Sampler & Model's in-built loss

--- utils.py
+++ utils.py
@@ -24,11 +24,11 @@ class SpreadSheetNLPCustomDataset(Dataset):
         #self.dataset = self.dataset[self.dataset['total_words']>256]
         #self.dataset = pd.DataFrame(pd.concat([Series(row['type'],row['posts'].split('|||')) for _, row in self.dataset.iterrows()]).reset_index())
         #self.dataset = self.dataset.rename(columns={k: cols_n[i] for i,k in enumerate(list(self.dataset.columns))})
-        #word_lengths = self.dataset['posts'].str.split()
-        #word_lengths = word_lengths.map(len)
-        #print("Exploding posts and types for large posts...\n")
-        #self.dataset = pd.DataFrame(pd.concat([Series(row['type'], chunkstring(row['posts'], 128)) for i, row in self.dataset.iterrows() if word_lengths[i] >= 128]).reset_index())
-        #self.dataset = self.dataset.rename(columns={k: cols_n[i] for i,k in enumerate(list(self.dataset.columns))})
+        word_lengths = self.dataset['posts'].str.split()
+        word_lengths = word_lengths.map(len)
+        print("Exploding posts and types for large posts...\n")
+        self.dataset = pd.DataFrame(pd.concat([Series(row['type'], chunkstring(row['posts'], 512)) for i, row in self.dataset.iterrows() if word_lengths[i] >= 512]).reset_index())
+        self.dataset = self.dataset.rename(columns={k: cols_n[i] for i,k in enumerate(list(self.dataset.columns))})
         self.dataset['total'] = self.dataset['posts'].str.split()
         self.dataset['total'] = self.dataset['total'].map(len)
         self.dataset = self.dataset[self.dataset['total']>=30]
