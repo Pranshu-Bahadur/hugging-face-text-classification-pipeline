1d87ddb5b61046a1f3dae5e300ff91069e391d8e
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-17 02:02:49 +0530

    test

--- experiment.py
+++ experiment.py
@@ -52,7 +52,7 @@ class Experiment(object):
                 prev_inertia_key = list(m_dict.keys())[-1]
                 m = lambda y1,x1: (curr_inertia - y1)/(k - x1)
                 difference = int(((m(highest_inertia_key, m_dict[highest_inertia_key]["k"])) - (m(prev_inertia_key, m_dict[prev_inertia_key]["k"]))))
-                flag = differences[-1] == difference if len(differences)>2 else False
+                self.classifier.writer.add_scalar("Gap",difference, k)
                 if len(differences)>2 and abs(differences[-1] - difference) < 10:
                     print(f"Elbow at {k-1}")
                     break
