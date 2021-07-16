cbe31c9a50b8c1fc8a3ef3d14ce114de9c2cf40a
Pranshu Bahadur <pranshu041@gmail.com>
2021-07-17 00:21:11 +0530

    test

--- experiment.py
+++ experiment.py
@@ -42,8 +42,8 @@ class Experiment(object):
         m_dict = {}
         differences = []
         for k in range(2, n+1):
-            print(torch.min(cluster_ids))
             cluster_ids, centers = kmeans(X=X, num_clusters = k, device=torch.device('cuda'))
+            print(torch.min(cluster_ids))
             curr_inertia = sum([torch.sum((X[cluster_ids==i].cpu() - centers[i].cpu())**2, dim=-1) for i in range(k)])
             print(curr_inertia)
             if k!=2:
