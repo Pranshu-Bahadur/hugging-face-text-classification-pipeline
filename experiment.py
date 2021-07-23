faaea5a8f59b5e7799368665daff8bd131602016
Mahidhar <tamminenimahi@gmail.com>
2021-07-23 16:53:59 +0530

    exp

--- experiment.py
+++ experiment.py
@@ -13,7 +13,7 @@ from torch.utils.data import WeightedRandomSampler
 class Experiment(object):
     def __init__(self, config):
         self.classifier = NLPClassifier(config)
-        #self.class_weights = np.array([])
+        self.class_weights = np.array([])
 
     def _run(self):
         splits = self._preprocessing(True)
@@ -21,9 +21,6 @@ class Experiment(object):
         print("Computing Weighted Random Sampler")
         sampler_loader = Loader(splits[0], self.classifier.bs, shuffle=False, num_workers=4)
         target = torch.cat([data["labels"] for data in sampler_loader])
-        print(len(target))
-        self.class_weights = np.array([0]*len(target))
-        print(len(self.class_weights))
         samples_weight = np.array([self.class_weights[t] for t in target])
         samples_weight = torch.from_numpy(samples_weight)
         samples_weight = samples_weight.double()
